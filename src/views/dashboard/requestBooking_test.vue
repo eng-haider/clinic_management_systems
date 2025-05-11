da01fa988dd2afe66db5fb089d39b161b8ccaf09
eng-haider <en.haider1@gmail.com>
2025-05-11 17:37:42 +0300

    ط

--- src/views/dashboard/requestBooking_test.vue
+++ src/views/dashboard/requestBooking_test.vue
@@ -271,9 +271,9 @@ export default {
   data() {
     return {
       // Calendar state
-      focus: "2025-04-01",
-      startDate: "2025-04-01",
-      endDate: "2025-05-30",
+      focus: "2025-05-01",
+      startDate: "2025-05-01",
+      endDate: "2025-06-30",
       reservations: [],
       
       // Dialog state
