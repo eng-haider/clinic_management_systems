02b0fbb59949e1fdaec74ab8b87653e394e37bc3
eng-haider <31736386+eng-haider@users.noreply.github.com>
2026-01-01 16:31:51 +0300

    dd
    dd

--- src/views/dashboard/requestBooking_test.vue
+++ src/views/dashboard/requestBooking_test.vue
@@ -271,9 +271,9 @@ export default {
   data() {
     return {
       // Calendar state
-      focus: "2025-12-01",
-      startDate: "2025-12-01",
-      endDate: "2026-01-01",
+      focus: "2026-01-01",
+      startDate: "2026-01-01",
+      endDate: "2027-01-01",
       reservations: [],
       
       // Dialog state
